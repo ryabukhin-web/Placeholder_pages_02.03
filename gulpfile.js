d0812209b171e36560c0e45a2fcc933310372d12
Ryabukhin_Nikita <ryabukhinikita@yandex.ru>
2021-03-02 11:20:05 +0300

    Final version 02.03;2021

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+const { src, dest, watch, parallel, series } = require('gulp');
+
+const sass          = require('gulp-sass');
+const concat        = require('gulp-concat');
+const browserSync   = require('browser-sync').create();
+const uglify        = require('gulp-uglify-es').default;
+const autoprefixer  = require('gulp-autoprefixer');
+const imagemin      = require('gulp-imagemin');
+const del           = require('del');
+
+function browsersync() {
+    browserSync.init({
+        server : {
+            baseDir: 'app/'
+        }
+    });
+}
+
+function cleanDist() {
+    return del('dist');
+}
+
+function images() {
+    return src('app/images/**/*')
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.mozjpeg({quality: 75, progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(dest('dist/images'))
+}
+
+function scripts() {
+    return src([
+        'node_modules/jquery/dist/jquery.js',
+        'app/js/main.js'
+    ])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('app/js'))
+        .pipe(browserSync.stream())
+}
+
+function styles() {
+    return src([
+        '14_Kompozitor/fonts/**/*.css',
+        '14_Kompozitor/*.sass'
+    ])
+            .pipe(sass({outputStyle: 'compressed'}))
+            .pipe(concat('style.min.css'))
+            .pipe(autoprefixer({
+                overrideBrowserslist: ['last 15 version'],
+                grid: true
+            }))
+            .pipe(dest('14_Kompozitor'))
+            .pipe(browserSync.stream())
+}
+
+function watching() {
+    watch(['14_Kompozitor/*.sass'], styles);
+    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+    watch(['14_Kompozitor/*.html']).on('change', browserSync.reload);
+}
+
+function build() {
+    return src([
+        'app/css/style.min.css',
+        'app/fonts/**/*',
+        'app/js/main.min.js',
+        'app/*.html'
+    ], {base: 'app'})
+        .pipe(dest('dist'))
+}
+
+exports.styles = styles;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.cleanDist = cleanDist;
+exports.images = images;
+
+exports.build = series(cleanDist, images, build);
+exports.default = parallel(browsersync, watching);
